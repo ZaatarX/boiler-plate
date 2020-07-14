985c3dda1175c64431dce0ed873ff969e8bbd216
Ricardo <fadlallah8@gmail.com>
2020-07-14 23:49:55 +0300

    fixing heroku deployment p4

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
 });
 
 // Set port server
