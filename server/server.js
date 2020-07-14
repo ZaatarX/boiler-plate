50ae0f49766e4141d76a0a8f3a48b88a64c1d6b8
Ricardo <fadlallah8@gmail.com>
2020-07-14 23:36:05 +0300

    fixing heroku deployment

--- server/server.js
+++ server/server.js
@@ -28,6 +28,15 @@ mongoose
 // Use Routes
 app.use("/api/users", require("./routes/api/users"));
 
+// Deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
 // Set port server
 const PORT = process.env.PORT || 5000;
 
